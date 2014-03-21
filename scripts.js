75dd3b62418b114e4166211f4bfc02fb9ce4b9ef
Jessica Duarte <jessynd@Jessicas-MacBook-Air.local>
2014-03-20 20:16:18 -0400

    nav option 1

---
+++ scripts.js
@@ -0,0 +1,20 @@
+var jp = {};
+
+// jp.menuClick = function(){
+// 	$("nav").on("click", function(){
+// 			$("nav ul").toggleClass("show");
+// 			$("nav li").toggleClass("smoothDown");
+// 	});
+// };
+
+
+
+
+
+
+$(function(){
+			$('a').smoothScroll({offset:-20});
+		
+
+		}); //end doc ready
+
